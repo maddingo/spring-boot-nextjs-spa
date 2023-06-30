b38be24f8a225308664067ba2368379e07e09014
Martin Goldhahn <martin.goldhahn@lyse.no>
2023-06-30 12:48:07 +0200

    route

--- src/main/js/next.config.js
+++ src/main/js/next.config.js
@@ -3,24 +3,24 @@ module.exports = {
 
   async rewrites() {
     return {
-      beforeFiles: [
+      // beforeFiles: [
         // These rewrites are checked after headers/redirects
         // and before all files including _next/public files which
         // allows overriding page files
-        {
+        // {
         //   source: '/api/',
         //   destination: 'http://localhost:8080/api/',
         //   has: [{ type: 'query', key: 'overrideMe' }],
-        },
-      ],
-      afterFiles: [
+        // },
+      // ],
+      // afterFiles: [
       //   These rewrites are checked after pages/public files
       //   are checked but before dynamic routes
-        {
+      //   {
         //   source: '/non-existent',
         //   destination: '/somewhere-else',
-        },
-      ],
+        // },
+      // ],
       fallback: [
         // These rewrites are checked after both pages/public files
         // and dynamic routes are checked
