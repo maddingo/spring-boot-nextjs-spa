33c813a5c3150c9fa0b4a820c18bd91076c8df92
Martin Goldhahn <martin.goldhahn@lyse.no>
2023-06-30 12:31:55 +0200

    added rewrite to backend

--- src/main/js/next.config.js
+++ src/main/js/next.config.js
@@ -1,6 +1,43 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // These rewrites are checked after headers/redirects
+        // and before all files including _next/public files which
+        // allows overriding page files
+        {
+        //   source: '/api/',
+        //   destination: 'http://localhost:8080/api/',
+        //   has: [{ type: 'query', key: 'overrideMe' }],
+        },
+      ],
+      afterFiles: [
+      //   These rewrites are checked after pages/public files
+      //   are checked but before dynamic routes
+        {
+        //   source: '/non-existent',
+        //   destination: '/somewhere-else',
+        },
+      ],
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: '/api/:path*',
+          destination: `http://localhost:8080/api/:path*`,
+        },
+      ],
+    }
+  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://localhost:8080/api/:path*',
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
+};
